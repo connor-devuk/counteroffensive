c968eaa935e79f865ef0c0728dedc5d10113d682
politeuk <connorgoodallbusiness@gmail.com>
2024-10-04 18:08:24 +0100

    dont open this commit

--- weapons/revolver.gd
+++ weapons/revolver.gd
@@ -13,26 +13,28 @@ func _process(_delta: float) -> void:
 		shoot()
 
 func shoot() -> void:
-	var raycast = player.raycast
+	var raycast = player.camera
 	var space_state = player.get_world_3d().direct_space_state
-    
-    # Get the global transform of the raycast
+	
+	# Get the global transform of the raycast
 	var ray_start = raycast.global_transform.origin
-	var ray_end = ray_start + raycast.global_transform.basis.z * 1000  # Extend the ray to 1000 units
-    
-    # Create physics ray query
+	# var ray_direction = -raycast.global_transform.basis.z
+	var ray_end = ray_start + -raycast.global_transform.basis.z * 1000  # Extend the ray to 1000 units
+	
+	# Create physics ray query
 	var query = PhysicsRayQueryParameters3D.create(ray_start, ray_end)
-    
-    # Perform the raycast
+	
+	# Perform the raycast
 	var result = space_state.intersect_ray(query)
-    
+	
 	if result:
-		print(result.collider.name)
+		print("Hit object: " + result.collider.name)
+		print("Hit position: " + str(result.position))
 		if result.collider.name == "hitbox":
 			print("Hit!")
 		else:
 			print("Miss!")
 	else:
 		print("Nothing in trajectory")
-    
+	
 	# print("Bang!")
